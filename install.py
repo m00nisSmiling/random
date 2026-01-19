eb779790f65813618f38af947bc1b4bbe5e3ae9f
moonissmiling <122709554+m00nisSmiling@users.noreply.github.com>
2026-01-19 20:14:35 +0700

    Update install.py

--- install.py
+++ install.py
@@ -166,6 +166,6 @@ def install():
 
 install()
 
-requests.post(f"https://api.telegram.org/bot{bot_key}/sendMessage",json={"chat_id": chatid, "text": f"[ Log2block setup done. Note: {note} ]"},timeout=5)
+requests.post(f"https://api.telegram.org/bot{botkey}/sendMessage",json={"chat_id": chatid, "text": f"[ Log2block setup done. Note: {note} ]"},timeout=5)
 os.system("mv ./uninstall.py /root/moni-uninstall.py")
 os.system("rm -rf ../log2block 2> /dev/null")
